9ec64d284fc5faed52173f0b43b62ec8feac5ceb
anjfranc <97130501+anjfranc@users.noreply.github.com>
2022-04-12 10:09:27 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -29,18 +29,8 @@ setTimeout(()=>{intro.style.top='-100vh';
     
 })
 
-var input = document.querySelector(".nav");
 
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    // event.preventDefault();
-    // Trigger the button element with a click
-    document.querySelector(".click").click();
-  }
-});
+
 
 
 })
